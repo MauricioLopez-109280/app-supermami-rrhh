44fca3614d70b35e4f868d65d452634f1f76e511
MauricioLopez-109280 <109280@tecnicatura.frc.utn.edu.ar>
2020-11-03 23:57:21 -0300

    ajustes

--- src/app/services/mami.service.ts
+++ src/app/services/mami.service.ts
@@ -38,38 +38,38 @@ export class MamiService {
         this.ListIntegrantes.push( 
             new IntegranteModel('Blanco','Carolina','QA', '110668@tecnicatura.frc.utn.edu.ar', 110668,
             'https://lh3.googleusercontent.com/a-/AOh14GhKx-z-zAyMZl3owmvQMOWw5ZaYCF3OA87uYPl93g=s70-p-k-rw-no',
-            'Hola soy caro'
+            'Hola soy Caro'
             ) 
         );
         this.ListIntegrantes.push( 
             new IntegranteModel('Maldonado','Santiago', 'Scrum Master', '110352@tecnicatura.frc.utn.edu.ar', 110352, 
             // 'https://media-exp1.licdn.com/dms/image/C4E03AQH0hbNPvgG8Tw/profile-displayphoto-shrink_200_200/0?e=1609977600&v=beta&t=edxJs71Nn2TGWOSiRyDKGAgha9AQ1m0EsJEDbuet_zs',
             'https://lh3.googleusercontent.com/a-/AOh14Gg6F632_8wBh4D6lCDvpQ5tddu0b-AxM0NlBmX8vm4=s70-p-k-rw-no',
-            'Mi pasion es dejar comentarios en google review'
+            'Hola soy Santi'
             ) 
         );
         this.ListIntegrantes.push( 
             new IntegranteModel('Lovecchio','Matías', 'Backend Developer', '110437@tecnicatura.frc.utn.edu.ar', 110437,
             'https://lh3.googleusercontent.com/a-/AOh14GhOnX0IL6XH1Itx7LV2OjOWBJSfre76kEgQKNER0g=s70-p-k-rw-no',
-            'Hola soy mati'
+            'Hola soy Mati'
             ) 
         );
         this.ListIntegrantes.push( 
             new IntegranteModel('Balsamo','Franco', 'DBA', '109921@tecnicatura.frc.utn.edu.ar', 109921,
             'https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/V8BNOaftJmYVauEn3GhlBizMuh99l-qFwCLcDEAEiGQoBZhD___________8BGMKv-fv______wE/s70-p-k-rw-no/photo.jpg',
-            'Hola soy franco'
+            'Hola soy Franco'
             ) 
         );
         this.ListIntegrantes.push( 
             new IntegranteModel('Pucheta','Ignacio', 'Frontend Developer', '110962@tecnicatura.frc.utn.edu.ar', 110962,
             'https://lh3.googleusercontent.com/a-/AOh14GjZljOp5FaW4n0wPTmDDcQIeCf6hWYWfPKiMw_s-6c=s70-p-k-rw-no',
-            'Hola soy ignacio'
+            'Hola soy Ignacio'
             ) 
         );
         this.ListIntegrantes.push( 
             new IntegranteModel('Lopez','Mauricio', 'Backend Developer', '109280@tecnicatura.frc.utn.edu.ar', 109280,
             'https://lh3.googleusercontent.com/a-/AOh14GhQ-hr3ShGdcD3HVWJ-qEOMxOVATdegWRYe7Jjkgw=s64',
-            'Hola soy mauri'
+            'Hola soy Mauri'
             ) 
         );
 
